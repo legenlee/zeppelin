39fb985d2e17220ded40964c84ff4742818254e5
LegenLee <lsko1332@naver.com>
2023-01-19 19:39:02 +0900

    chore: Add material design icons

--- src/renderer/plugins/vuetify.ts
+++ src/renderer/plugins/vuetify.ts
@@ -2,8 +2,16 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 
 export const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   components,
   directives,
 });
