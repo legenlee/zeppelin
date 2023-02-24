c0207ce6a3bb210d69d94f5b7fa2899686a91b9f
LegenLee <lsko1332@naver.com>
2023-02-24 19:46:57 +0900

    fix: Remove main event to call api method directly

--- src/main/game/version.ts
+++ src/main/game/version.ts
@@ -1,25 +1,9 @@
-import { ipcMain } from 'electron';
-import { Games } from 'core/api/games';
-
-export enum VersionChannelName {
-  GET_MINECRAFT_VERSIONS = 'version.getMinecraftVersions',
-}
-
 export class Version {
   private constructor() {
     //
   }
 
-  public async getMinecraftVersions() {
-    return await Games.getAvailableVersions();
-  }
-
   public static bootstrap(): void {
     const instance = new Version();
-
-    ipcMain.handle(
-      VersionChannelName.GET_MINECRAFT_VERSIONS,
-      instance.getMinecraftVersions
-    );
   }
 }
