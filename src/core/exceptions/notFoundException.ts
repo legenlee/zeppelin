d78c7d40c2e73397e4876b5a19cafccac35a22a3
LegenLee <lsko1332@naver.com>
2023-03-08 19:20:45 +0900

    fix: Remove name initialization of notfoundexception class

--- src/core/exceptions/notFoundException.ts
+++ src/core/exceptions/notFoundException.ts
@@ -3,6 +3,5 @@ import { BaseException } from './baseException';
 export class NotFoundException extends BaseException {
   public constructor(message = 'No installed instance is found.') {
     super(message);
-    this.name = NotFoundException.name;
   }
 }
