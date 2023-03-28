e9f65412291a4d25fb98cf0a6abc9c9412f07af1
LegenLee <lsko1332@naver.com>
2023-03-28 17:21:35 +0900

    fix: Rename meta.json to versions.json

--- src/core/common/constants.ts
+++ src/core/common/constants.ts
@@ -1,7 +1,15 @@
 export class Constants {
   public static readonly IDENTIFIER = 'Zeppelin';
-  public static readonly META_FILE_NAME = 'meta.json';
+
   public static readonly ASSETS_DIRECTORY_NAME = 'assets';
   public static readonly LIBRARIES_DIRECTORY_NAME = 'libraries';
   public static readonly VERSIONS_DIRECTORY_NAME = 'versions';
+
+  public static readonly VERSIONS_FILE_NAME = 'versions.json';
+
+  /**
+   * @deprecated
+   * META_FILE_NAME is deprecated. Use {@link Constants.VERSIONS_FILE_NAME VERSIONS_FILE_NAME} instead.
+   */
+  public static readonly META_FILE_NAME = Constants.VERSIONS_FILE_NAME;
 }
