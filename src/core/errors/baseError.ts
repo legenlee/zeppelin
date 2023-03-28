5d7386adb721b3084b3e7ce6c7d36500616ae9ef
LegenLee <lsko1332@naver.com>
2023-03-28 18:38:07 +0900

    refactor: Make baseError to abstract class

--- src/core/errors/baseError.ts
+++ src/core/errors/baseError.ts
@@ -1,6 +1,6 @@
 import { Constants } from 'core/common/constants';
 
-export class BaseError extends Error {
+export abstract class BaseError extends Error {
   protected constructor(message: string) {
     super(`[${Constants.IDENTIFIER}]: ${message}`);
     this.name = this.constructor.name;
