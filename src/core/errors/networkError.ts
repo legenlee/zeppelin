f65958d9a8590119d41200e474e3955567031287
LegenLee <lsko1332@naver.com>
2023-03-28 18:38:21 +0900

    feat: Implement base network error class

--- src/core/errors/networkError.ts
+++ src/core/errors/networkError.ts
@@ -1,3 +1,16 @@
 import { BaseError } from './baseError';
 
-export class NetworkError extends BaseError {}
+export class NetworkError extends BaseError {
+  public readonly statusCode: number;
+
+  public constructor(
+    message: string,
+    statusCode: string | number,
+    stack?: string
+  ) {
+    super(message);
+    this.stack = stack;
+    this.statusCode =
+      typeof statusCode === 'string' ? parseInt(statusCode) : statusCode;
+  }
+}
