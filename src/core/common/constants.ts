ce0767333e780eab7c8413eab3653ab952ac23a1
LegenLee <lsko1332@naver.com>
2023-03-28 16:01:47 +0900

    fix: Seperate assets managing logic from launcher to installer

--- src/core/common/constants.ts
+++ src/core/common/constants.ts
@@ -1,4 +1,7 @@
 export class Constants {
   public static readonly IDENTIFIER = 'Zeppelin';
   public static readonly META_FILE_NAME = 'meta.json';
+  public static readonly ASSETS_DIRECTORY_NAME = 'assets';
+  public static readonly LIBRARIES_DIRECTORY_NAME = 'libraries';
+  public static readonly VERSIONS_DIRECTORY_NAME = 'versions';
 }
--- src/core/game/installer.ts
+++ src/core/game/installer.ts
@@ -1,5 +1,29 @@
+import { Constants } from 'core/common/constants';
+import path from 'path';
+
+// TODO: 런처 클래스로부터 파일 설치 로직 분리
+
+/**
+ * Installer class handling Minecraft assets.
+ */
 export class Installer {
-  public constructor() {
-    //
+  private _baseDirectory: string;
+
+  public get baseDirectory() {
+    return this._baseDirectory;
   }
+
+  public get assetsDirectory() {
+    return path.join(this._baseDirectory, Constants.ASSETS_DIRECTORY_NAME);
+  }
+
+  public get librariesDirectory() {
+    return path.join(this._baseDirectory, Constants.LIBRARIES_DIRECTORY_NAME);
+  }
+
+  public get versionsDirectory() {
+    return path.join(this._baseDirectory, Constants.VERSIONS_DIRECTORY_NAME);
+  }
+
+  public static getAvailableVersions() {}
 }
