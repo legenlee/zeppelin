aca28e14ff82248957b96ce342b6e077957017e5
LegenLee <lsko1332@naver.com>
2023-05-22 17:45:20 +0900

    refactor: Rename version to manifest

--- src/common/api/metadata.ts
+++ src/common/api/metadata.ts
@@ -1,13 +1,11 @@
 import { Client } from 'common/client';
 import { Constants } from 'common/utils/constants';
 
-export class Version {
-  private static readonly _client = Client.create(
-    Constants.LAUNCHER_METADATA_URL
-  );
+export class Metadata {
+  private static _client = Client.create(Constants.METADATA_URL);
 
   public static async getLauncherMetadata(): Promise<Record<string, unknown>> {
-    const response = await Version._client.get(
+    const response = await Metadata._client.get(
       '/mc/game/version_manifest.json'
     );
 
