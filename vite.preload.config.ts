0cf5f1e2b1987c02bb975d900b0bf128cba15b0e
LegenLee <lsko1332@naver.com>
2023-05-25 18:01:04 +0900

    chore: add absolute alias

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import { restart } from './restart';
 
 export default defineConfig({
   plugins: [restart()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src'),
+      },
+    ],
+  },
 });
