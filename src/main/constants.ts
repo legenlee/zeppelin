741d162e5e50c37231ed6476bc20cc543e0a88f5
LegenLee <lsko1332@naver.com>
2023-12-04 19:13:00 +0900

    feat: Add constant values

---
+++ src/main/constants.ts
@@ -0,0 +1,10 @@
+export const constants = {
+  common: {
+    appId: 'zeppelin',
+  },
+  api: {
+    game: 'https://piston-meta.mojang.com/mc/game/version_manifest_v2.json',
+    libraries: 'https://libraries.minecraft.net',
+    assets: 'https://resources.download.minecraft.net',
+  },
+} as const;
